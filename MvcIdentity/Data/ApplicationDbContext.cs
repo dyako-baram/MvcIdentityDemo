3562d477645ea54419293bcc4c62b1a4fcf4f302
Dyako Baram <55919885+dyako-baram@users.noreply.github.com>
2021-03-24 09:54:33 +0300

    Add project files.

---
+++ MvcIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcIdentity.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IdentityUser>()
+                .Ignore(c => c.PhoneNumber)
+                .Ignore(c => c.PhoneNumberConfirmed)
+                .Ignore(c => c.TwoFactorEnabled);
+
+            modelBuilder.Entity<IdentityUser>().ToTable("Users");
+            modelBuilder.Entity<IdentityRole>().ToTable("Role");
+            modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim");
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim");
+            modelBuilder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
+
+        }
+    }
+}
---
+++ MvcIdentity/README.md
@@ -0,0 +1,11 @@
+# MVC IDENTITY
+
+insted of application.json, secrets.json is used like this
+
+```json
+{
+  "ConnectionStrings": {
+    "Default": "<sqlserver connection string>"
+  }
+}
+```
